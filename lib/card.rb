a4c3b3e1a5bbc37c9bfde932188236c20d4c7b0c
Coleman and Brian <coleman+brian@appacademy.io>
2013-11-01 11:44:26 -0400

    Card initialize and display hashes

---
+++ lib/card.rb
@@ -0,0 +1,24 @@
+require_relative 'errors'
+class Card
+
+  DISPLAY_VALUE = {
+                    11 => 'J',
+                    12 => 'Q',
+                    13 => 'K'
+                  }
+
+  DISPLAY_SUIT = {
+                  :heart => "♡",
+                  :diamond => "♢",
+                  :club => "♣",
+                  :spade => "♠"
+                }
+
+  attr_readers :value, :suit
+
+  def initialize(value, suit)
+    raise NoSuitError unless DISPLAY_SUIT.keys.include?(suit)
+    raise NoValueError unless (1..13).include?(value)
+    @value, @suit = value, suit
+  end
+end
