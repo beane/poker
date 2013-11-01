395bc08b5051077eb2b2913b57509f23cd051252
Coleman and Brian <coleman+brian@appacademy.io>
2013-11-01 11:29:44 -0400

    start card specs

---
+++ spec/card_spec.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+require 'rspec'
+require 'card'
+
+describe Card do
+  subject(:card) { Card.new(12, :hearts) }
+
+  it 'has a value' do
+    expect(card.value).to be truthy
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to be truthy
+  end
+
+  it 'only has legit suits' do
+    expect { Card.new(10, :starks) }.to raise_error(NoSuitError)
+  end
+
+  it 'only has legit values' do
+    expect { Card.new(76, :hearts) }.to raise_error(NoValueError)
+  end
+
+  it 'displays a unicode card' do
+    expect(card.display).to eq "♡K♡"
+  end
+
+
+end
